b5bd9395f34cc1d88fa195a31abce634b8dccd6f
josue <jcanaviri20@gmail.com>
2022-01-30 14:49:33 -0400

    Implementing add notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
 import { Notes } from './components/Notes'
 
 export const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: uuidv4(),
+      task: 'Learn React',
+    },
+    {
+      id: uuidv4(),
+      task: 'Do laundry',
+    },
+  ])
+
+  const addNote = () => {
+    setNotes([...notes, { id: uuidv4(), task: 'New task' }])
+  }
+
   return (
     <>
-      <button onClick={() => console.log('añadir nota')}>+</button>
-      <Notes />
+      <button onClick={addNote}>+</button>
+      <Notes notes={notes} />
     </>
   )
 }
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,19 +1,6 @@
 import React from 'react'
-import { v4 as uuidv4 } from 'uuid'
 
-const notes = [
-  {
-    // id: '4e81fc6e-bfb6-419b-93e5-0242fb6f3f6a',
-    id: uuidv4(),
-    task: 'Learn React',
-  },
-  {
-    id: uuidv4(),
-    task: 'Do laundry',
-  },
-]
-
-export const Notes = () => {
+export const Notes = ({ notes }) => {
   return (
     <ul>
       {notes.map((note) => (
